3e6d93ed9206a63395726984fe44bd7cf3fcb4b0
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-05 23:23:37 -0800

    implement models for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # related_name of posts, we can access category.posts to give us a list of posts with that category
+    categories = models.ManyToManyField('Category', related_name='posts')
+
+# allow users to make comments 
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    # many to one relationship defined with foreign key
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
