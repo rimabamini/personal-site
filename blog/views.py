eb6a1f4569180857d74f096d3560b48c091f697e
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-06 01:29:09 -0800

    implement views for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def blog_index(request):
+    # import post model, import Queryset containing all posts in database
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+# take a category name and query the Post database for all posts 
+def blog_category(request, category):
+    # filter post that contain category name corresponding to argument
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
+
+# requires form, takes primary key value as argument to retrieve object 
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
