7e4a58997fd556d5546cf318230d5f8a4529ea18
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-05 18:08:59 -0800

    implement views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+# index view will show a snippet of information about each project 
+def project_index(request):
+    # perform query to retrieve all objcts in projects table 
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+# detail view will show more information on a particular topic 
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
